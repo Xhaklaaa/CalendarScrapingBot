5ba447a4a13a13bb0951432499093ec52e2c089c
Xhaklaaa <Xhaklaaa@gmail.com>
2023-09-30 17:54:19 +0700

    Create intro message

--- app/bot.go
+++ app/bot.go
@@ -1,34 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func main() {
-	bot, err := tgbotapi.NewBotAPI("6635332877:AAE_EA1Rvlfw6sLLJxfhIyRrDTtmyd20PG8")
+var gBot *tgbotapi.BotAPI
+var gToken string
+var gChatId int64
+
+func init() {
+	_ = os.Setenv(TOKEN_NAME_IN_OS, "6635332877:AAE_EA1Rvlfw6sLLJxfhIyRrDTtmyd20PG8")
+
+	if gToken = os.Getenv(TOKEN_NAME_IN_OS); gToken == "" {
+		panic(fmt.Errorf(`failed to load enviroment variable "%s"`, TOKEN_NAME_IN_OS))
+	}
+	var err error
+	gBot, err := tgbotapi.NewBotAPI("gToken")
 	if err != nil {
 		log.Panic(err)
 	}
+	gBot.Debug = true
+}
 
-	bot.Debug = true
+func isStartMessage(update *tgbotapi.Update) bool {
+	return update.Message != nil && update.Message.Text == "/start"
+}
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+func main() {
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+	log.Printf("Authorized on account %s", gBot.Self.UserName)
 
-	updates := bot.GetUpdatesChan(u)
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
 
-	for update := range updates {
-		if update.Message != nil { // If we got a message
+	for update := range gBot.GetUpdatesChan(updateConfig) {
+		if isStartMessage(&update) {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 			msg.ReplyToMessageID = update.Message.MessageID
 
-			bot.Send(msg)
+			gBot.Send(msg)
 		}
+		//TODO: implement proccessing other types of updates
 	}
 }
--- data/contants.go
+++ data/contants.go
@@ -1,5 +1,10 @@
 package main
 
-conts(
-	TOKEN_NAME_IN_OS = "BotaZamena"
+const (
+	TOKEN_NAME_IN_OS      = "BotaZamenabot"
+	UPDATE_CONFIG_TIMEOUT = 60
+
+	EMOJI_BLOWING_KISS = "\U0001F618" // 😘
+	EMOJI_HUGGING      = "\U0001F917" // 🤗
+
 )
